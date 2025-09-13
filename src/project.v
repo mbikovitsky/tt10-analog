26aecb7667ab55a76d67b7fc55d0210b1162d013
Michael Bikovitsky <biko@bikodbg.com>
2025-09-13 18:24:03 +0300

    Fix LVS scripts to work with the complete design

--- src/project.v
+++ src/project.v
@@ -1,11 +1,6 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
-module tt_um_example (
+module tt_um_mbikovitsky_audio_player (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
@@ -19,5 +14,56 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+   wire [15:0] o_digital;
+   digital_top top (.VGND(VGND),
+                    .VPWR(VDPWR),
+                    .clk(clk),
+                    .rst_n(rst_n),
+                    .ui_in(ui_in),
+                    .uo_out(uo_out),
+                    .uio_in(uio_in),
+                    .uio_out(uio_out),
+                    .uio_oe(uio_oe),
+                    .o_digital(o_digital)
+                    );
+
+   wire dac_1_buf_0;
+   dac dac_1 (.VDD(VDPWR),
+              .VSS(VGND),
+              .out(dac_1_buf_0),
+              .a0(o_digital[8]),
+              .a1(o_digital[9]),
+              .a2(o_digital[10]),
+              .a3(o_digital[11]),
+              .a4(o_digital[12]),
+              .a5(o_digital[13]),
+              .a6(o_digital[14]),
+              .a7(o_digital[15])
+              );
+   buffer buffer_0 (.VDD(VDPWR),
+                    .VSS(VGND),
+                    .in(dac_1_buf_0),
+                    .out(ua[0])
+                    );
+
+   wire dac_2_buf_2;
+   dac dac_2 (.VDD(VDPWR),
+              .VSS(VGND),
+              .out(dac_2_buf_2),
+              .a0(o_digital[0]),
+              .a1(o_digital[1]),
+              .a2(o_digital[2]),
+              .a3(o_digital[3]),
+              .a4(o_digital[4]),
+              .a5(o_digital[5]),
+              .a6(o_digital[6]),
+              .a7(o_digital[7])
+              );
+   buffer buffer_2 (.VDD(VDPWR),
+                    .VSS(VGND),
+                    .in(dac_2_buf_2),
+                    .out(ua[1])
+                    );
 
 endmodule
